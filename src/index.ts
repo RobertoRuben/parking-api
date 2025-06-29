0efc8e7999d49740f33613f5b2ff8d67c975d793
RobertoRuben <robertoch263@gmail.com>
2025-06-29 17:28:53 -0500

    ✨ feat(database): initialize database connection and update server startup logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import "reflect-metadata";
+import * as dotenv from "dotenv";
 import app from "./app";
+import { AppDataSource } from "./database/db";
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`🚀 Server running on http://${process.env.HOST || "localhost"}:${process.env.PORT || 3000}`);
-});
+dotenv.config();
+
+async function main() {
+  try {
+    await AppDataSource.initialize();
+    console.log("✅ Database connected successfully");
+    app.listen(process.env.API_PORT, () => {
+      console.log(`🚀 Server is running on http://${process.env.API_HOST}:${process.env.API_PORT}`);
+    });
+  } catch (error){
+    console.error("❌ Error connecting to the database:", error);
+  }
+}
+
+main();
