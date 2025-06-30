a47903d25834c4e26d79b6853893e095c8f11017
RobertoRuben <robertoch263@gmail.com>
2025-06-30 01:11:16 -0500

    🐛 fix(vehicle): remove Vehicle entity and repository implementations

--- src/vechicle/repository/interface/vehicle-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Vehicle } from "@/vechicle/entity/vehicle";
-
-export interface VehicleRepository {
-    findById(id: number): Promise<Vehicle | null>;
-    findByLicensePlate(licensePlate: string): Promise<Vehicle | null>;
-    save(vehicle: Vehicle): Promise<Vehicle>;
-    deleteById(id: number): Promise<void>;
-    getPage(page: number, pageSize: number): Promise<Vehicle[]>;
-}
