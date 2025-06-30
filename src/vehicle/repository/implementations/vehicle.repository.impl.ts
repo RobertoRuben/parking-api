c56b7cb3296118dd4b42a7b0b3caa85f8ba77bce
RobertoRuben <robertoch263@gmail.com>
2025-06-30 01:14:48 -0500

    ✨ feat(vehicle): implement VehicleRepositoryImpl with methods for managing vehicles

---
+++ src/vehicle/repository/implementations/vehicle.repository.impl.ts
@@ -0,0 +1,44 @@
+import { Repository } from "typeorm";
+import { VehicleRepository } from "@/vehicle/repository/interface/vehicle.repository";
+import { Vehicle } from "@/vehicle/entity/vehicle.entity";
+import { AppDataSource } from "@/database/db";
+
+export class VehicleRepositoryImpl implements VehicleRepository {
+  private repository: Repository<Vehicle>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(Vehicle);
+  }
+
+  async findById(id: number): Promise<Vehicle | null> {
+    return await this.repository.findOne({
+      where: { id },
+    });
+  }
+
+  async findByLicensePlate(licensePlate: string): Promise<Vehicle | null> {
+    return await this.repository.findOne({
+      where: { licensePlate },
+    });
+  }
+
+  async save(vehicle: Vehicle): Promise<Vehicle> {
+    return await this.repository.save(vehicle);
+  }
+
+  async deleteById(id: number): Promise<void> {
+    await this.repository.delete(id);
+  }
+
+  async getPage(page: number, pageSize: number): Promise<Vehicle[]> {
+    const skip = (page - 1) * pageSize;
+
+    return await this.repository.find({
+      skip,
+      take: pageSize,
+      order: {
+        createdAt: "DESC",
+      },
+    });
+  }
+}
